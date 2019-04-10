28956cf19970bf264e7770e499c90058a6b68300
Huub van Thienen <h.a.van.thienen@hva.nl>
2019-04-10 13:56:32 +0200

    Documentatie up-to-date gemaakt.

--- src/main/java/controller/NewQuestionController.java
+++ src/main/java/controller/NewQuestionController.java
@@ -1,14 +1,15 @@
 package controller;
 
 import javafx.event.ActionEvent;
+import model.entity.Question;
 import model.entity.Quiz;
 
 public class NewQuestionController {
 
-  public void setup(Quiz quiz) {}
+  public void setup(Quiz quiz, Question question) {}
 
   public void doMenu(ActionEvent event) {}
 
   public void doNewQuestion(ActionEvent event) {}
-  
+
 }
