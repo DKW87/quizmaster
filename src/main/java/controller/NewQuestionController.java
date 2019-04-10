3dc11bb60369c5bc3893800d4f6cb3eda0e50369
Huub van Thienen <h.a.van.thienen@hva.nl>
2019-04-10 13:33:50 +0200

    NewQuestion gemaakt.

--- src/main/java/controller/NewQuestionController.java
+++ src/main/java/controller/NewQuestionController.java
@@ -1,4 +1,14 @@
 package controller;
 
+import javafx.event.ActionEvent;
+import model.entity.Quiz;
+
 public class NewQuestionController {
+
+  public void setup(Quiz quiz) {}
+
+  public void doMenu(ActionEvent event) {}
+
+  public void doNewQuestion(ActionEvent event) {}
+  
 }
