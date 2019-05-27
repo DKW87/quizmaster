4023015dd21539085ecd914c9ae069f02fcd04f9
Huub <huub@timeless-way.nl>
2019-05-27 17:17:21 +0200

    Onvolledige commit aangevuld.

--- src/main/java/controller/SelectQuizForStudentController.java
+++ src/main/java/controller/SelectQuizForStudentController.java
@@ -1,6 +1,14 @@
 package controller;
 
+import javafx.fxml.FXML;
+import javafx.scene.control.Button;
+
 public class SelectQuizForStudentController {
+
+  @FXML
+  private Button doQuizButton;
+
+  public void doQuiz() {}
 
   public void setup() {
   }
