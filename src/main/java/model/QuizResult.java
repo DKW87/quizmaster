624adf8214952bae0ccc87c48dffbb3ed6b2fb05
Zahir Ekrem <63649974+zahirekrem09@users.noreply.github.com>
2024-06-11 22:28:24 +0200

    feat(#6-#14) : Add QuizResult model class

--- src/main/java/model/QuizResult.java
+++ src/main/java/model/QuizResult.java
@@ -1,3 +1,79 @@
 package model;
 
-public class QuizResult {}
+import java.time.LocalDate;
+
+/**
+ * @author Zahir Ekrem SARITEKE
+ * @project quizmaster
+ * @created 11 juni 2024 - 22:15
+ */
+public class QuizResult implements Comparable<QuizResult> {
+
+
+    private int resultId;
+    private LocalDate date;
+    private User student;
+    private Quiz quiz;
+    private int score;
+
+    // ? All args constructor
+    public QuizResult(int resultId, User student, Quiz quiz, int score) {
+        this.resultId = resultId;
+        this.student = student;
+        this.quiz = quiz;
+        this.score = score;
+        this.date = LocalDate.now();
+    }
+
+    public int getResultId() {
+        return resultId;
+    }
+
+    public void setResultId(int resultId) {
+        this.resultId = resultId;
+    }
+
+    public LocalDate getDate() {
+        return date;
+    }
+
+    public void setDate(LocalDate date) {
+        this.date = date;
+    }
+
+    public User getStudent() {
+        return student;
+    }
+
+    public void setStudent(User student) {
+        this.student = student;
+    }
+
+    public Quiz getQuiz() {
+        return quiz;
+    }
+
+    public void setQuiz(Quiz quiz) {
+        this.quiz = quiz;
+    }
+
+    public int getScore() {
+        return score;
+    }
+
+    public void setScore(int score) {
+        this.score = score;
+    }
+
+    /**
+     * Compares this QuizResult object with the specified QuizResult object for order.
+     *
+     * @param  obj   the QuizResult object to be compared.
+     * @return      a negative integer, zero, or a positive integer as this QuizResult
+     *              is less than, equal to, or greater than the specified QuizResult.
+     */
+    @Override
+    public int compareTo(QuizResult obj) {
+        return this.date.compareTo(obj.date);
+    }
+}
