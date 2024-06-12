3bf0f9489ee3527a77bfea12c07dae2ad7f0dff9
Rob Jansen <rob.jansen2@hva.nl>
2024-06-12 10:45:47 +0200

    Class Quiz Ready ROB

--- src/main/java/model/Quiz.java
+++ src/main/java/model/Quiz.java
@@ -29,45 +29,59 @@ public class Quiz {
     public int getQuizId() {
         return quizId;
     }
+
     public void setQuizId(int quizId) {
         this.quizId = quizId;
     }
+
     public String getName() {
         return name;
     }
+
     public void setName(String name) {
         this.name = name;
     }
+
     public List<Question> getQuestions() {
         return questions;
     }
+
     public void setQuestions(List<Question> questions) {
         this.questions = questions;
     }
+
     public int getPassMark() {
         return PASS_MARK;
     }
+
     public void setPassMark(int passMark) {
         PASS_MARK = passMark;
     }
 
 
+    // method to add questions to quiz
+    // !NB: assumed the content of a Quiz will not be stored in the DB - only the result of the Quiz - otherwise add method to DAO
+    // Selection for right questions for the right quiz not yet added to method (TODO Rob)
+    // method input in UML change from Quiz quiz to Question question?
+    public void addQuestion(Question question) {
+        questions.add(question);
+    }
 
 
-// toString
-@Override
+    // toString
+    @Override
     public String toString() {
-        int QCounter =0;
+        int QCounter = 0;
         StringBuilder toStringQuiz = new StringBuilder();
-        toStringQuiz.append("Quiz ID: " + quizId + " ");
-        toStringQuiz.append("Name: " + name + "\n");
+        toStringQuiz.append("Quiz ID: " + this.quizId + " ");
+        toStringQuiz.append("Name: " + this.name + "\n");
         toStringQuiz.append("Questions: ");
-        for (Question question : questions) {
+        for (Question question : this.questions) {
             QCounter++;
             toStringQuiz.append("Question: " + QCounter + "\n");
             toStringQuiz.append(question.toString() + "\n");
         }
         return toStringQuiz.toString();
-}
+    }
 
 }
