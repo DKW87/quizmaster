bf32b06a5b345124af8ed2558d916e55004031c5
Danny Kwant <danny.kwant@hva.nl>
2024-06-12 14:35:33 +0200

    feat(#10-#13): Add Difficulty, Question model class

---
+++ src/main/java/model/Difficulty.java
@@ -0,0 +1,43 @@
+package model;
+
+/**
+ * @author Danny KWANT
+ * @project Quizmaster
+ * @created 12/06/2024 - 14:31
+ */
+public class Difficulty {
+
+    // attributes
+
+    private int difficultyId;
+    private String name;
+
+    // constructors
+
+    public Difficulty(String name) {
+        this.name = name;
+    }
+
+    // methods
+
+    // TODO @Danny
+
+    // getters and setters
+
+    public int getDifficultyId() {
+        return difficultyId;
+    }
+
+    public void setDifficultyId(int difficultyId) {
+        this.difficultyId = difficultyId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+} // class
