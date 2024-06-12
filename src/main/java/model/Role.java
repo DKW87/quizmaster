49d7f2f04984fc46a68c29f61043f4965d8881c5
Mack Bakkum <154362357+MackBak@users.noreply.github.com>
2024-06-12 07:33:18 +0200

    model: Add User and Role

---
+++ src/main/java/model/Role.java
@@ -0,0 +1,38 @@
+/**
+ * @author Mack Bakkum
+ * @project QuizMaster
+ * @created 12/06/24 - 07:25
+ */
+
+package model;
+
+public class Role {
+
+    private int roleId;
+    private String name;
+
+    public Role(String name) {
+        this.name = name;
+        this.roleId = 0; //Default value set to 0. Use setRoleId to change value per role.
+    }
+
+    public String toString(){
+        return "Role ID + Name: " + roleId + " " + name;
+    }
+
+    public int getRoleId() {
+        return roleId;
+    }
+
+    public void setRoleId(int roleId) {
+        this.roleId = roleId;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+}
