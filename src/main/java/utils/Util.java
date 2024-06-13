dbaf46656f52c82b4cd5cd9bff7eaabff332e417
Zahir Ekrem <63649974+zahirekrem09@users.noreply.github.com>
2024-06-13 08:51:04 +0200

    feat : Add Util class for convertCvsToArray

---
+++ src/main/java/utils/Util.java
@@ -0,0 +1,52 @@
+package utils;
+
+import model.Course;
+import model.Difficulty;
+import model.User;
+
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.Scanner;
+
+/**
+ * @author Zahir Ekrem SARITEKE
+ * @project quizmaster
+ * @created 12 Haziran Çarşamba 2024 - 21:13
+ */
+public class Util {
+    //    private final UserDAO  userDao = new UserDAO(dbAccess);
+    //    private final DifficultyDAO difficultyDao = new DifficultyDAO(dbAccess);
+
+    public static List<String> convertCvsToArray(String cvsPath) {
+        List<String> cvsList = new ArrayList<>();
+        File cvsFile = new File(cvsPath);
+        try {
+            Scanner fileReader   = new Scanner(cvsFile);
+            while (fileReader.hasNextLine()) {
+                cvsList.add(fileReader.nextLine());
+            }
+        } catch (FileNotFoundException e) {
+            System.out.println("Bestand niet gevonden" + e.getMessage());
+        }
+        return cvsList;
+    }
+
+    public static List<Course> generateCvsListToCourses(List<String> cvsList) {
+        List<Course> cursusLijst = new ArrayList<>();
+        if (!cvsList.isEmpty()) {
+            for (String string : cvsList) {
+                String[] regelArray = string.split(",");
+                String name = regelArray[0];
+                String difficultyName = regelArray[1];
+                // FIXME : userDao and difficultyDao not implemented
+//                User coordinator = userDao.findByUserName((regelArray[2]));
+//                Difficulty difficulty = difficultyDao.findByName(difficultyName);
+//                cursusLijst.add(new Course(name, difficultyName, coordinator));
+            }
+        }
+        return cursusLijst;
+    }
+
+}
