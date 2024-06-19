c23878aa709e058fa361e5541f70430ba58dbd4e
Zahir Ekrem <63649974+zahirekrem09@users.noreply.github.com>
2024-06-19 23:11:40 +0200

    refactor : Add global alert method

--- src/main/java/utils/Util.java
+++ src/main/java/utils/Util.java
@@ -2,12 +2,14 @@ package utils;
 
 import database.mysql.*;
 import javafx.scene.control.Alert;
+import javafx.scene.control.ButtonType;
 import model.*;
 import view.Main;
 
 import java.io.*;
 import java.util.ArrayList;
 import java.util.List;
+import java.util.Optional;
 import java.util.Scanner;
 
 /**
@@ -145,6 +147,15 @@ public class Util {
         alert.setContentText(message);
         alert.setContentText(contentText);
         alert.show();
+    }
+    public static boolean confirmMessage(String title,String message) {
+        Alert alert = new Alert(javafx.scene.control.Alert.AlertType.CONFIRMATION);
+        alert.setTitle(title);
+        alert.setHeaderText(null);
+        alert.setContentText(message);
+        Optional<ButtonType> option = alert.showAndWait();
+        return option.get().equals(ButtonType.OK);
+
     }
 
 
