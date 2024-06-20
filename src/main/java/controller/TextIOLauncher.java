ea3c4f60039c37c8152291519c51585e8f3f323d
Zahir Ekrem <63649974+zahirekrem09@users.noreply.github.com>
2024-06-20 10:02:31 +0200

    refactor : Updated TextIOLauncher

--- src/main/java/controller/TextIOLauncher.java
+++ src/main/java/controller/TextIOLauncher.java
@@ -19,38 +19,34 @@ import static utils.Util.*;
  * @project quizmaster
  * @created 13 June 2024 - 11:08
  */
-public  class TextIOLauncher {
-private final static DBAccess dbAccess = Main.getdBaccess();
+public class TextIOLauncher {
+    private final static DBAccess dbAccess = Main.getdBaccess();
 
-/*
 
     public static void main(String[] args) throws SQLException, IOException {
-        CourseDAO courseDao = new CourseDAO(dbAccess);
 
         // Stap 1: bulk create gebruikers TODO: @MacK
-        UserDAO userDAO = new UserDAO(dbAccess);
-        // Small CSV to check import into Java.
-        String csvFilePath = "resources/Gebruikers.csv";
-        List<User> importUsersTest = convertListToUsers(csvFilePath);
-        // For loop to import all users to the DB.
-        for (User user : importUsersTest) {
-            userDAO.storeOne(user);
-            System.out.printf("User %s added to DB\n", user.getUserName());
-        }
+        bulkCreateUsers();
 
-         //Stap 2: bulk create courses
+        //Stap 2: bulk create courses TODO: @Ekrem
         bulkCreateCourses();
 
         // Stap 3: bulk create Quizzen TODO: @Rob
-        String locationQuestionCSV = "Resources/Quizzen.csv";
-        QuizDAO quizDAO = new QuizDAO(dbAccess);
-        List<String> quizInCsv = convertCsvToArray(locationQuestionCSV);
-        List<Quiz> quizList = convertQuizListToObjects(quizInCsv);
-        for (Quiz quiz : quizList) {
-            quizDAO.storeOne(quiz);
-        }
+        bulkCreateQuizzes();
 
         // Stap 4: bulk create vragen TODO: @Danny
+        bulkCreateQuestions();
+
+    }
+
+    private static void bulkCreateCourses() throws SQLException {
+        CourseDAO courseDao = new CourseDAO(dbAccess);
+        List<Course> courses = generateCsvListToCourses(convertCsvToArray("resources/Cursussen.csv"));
+        courseDao.bulkCreate(courses);
+        System.out.println("Cursussen succesvol toegevoegd");
+    }
+
+    private static void bulkCreateQuestions() throws SQLException {
         String questionCsv = "Resources/Vragen.csv";
         QuestionDAO questionDao = new QuestionDAO(dbAccess);
         List<String> questionInCsv = convertCsvToArray(questionCsv);
@@ -58,13 +54,29 @@ private final static DBAccess dbAccess = Main.getdBaccess();
         for (Question question : questionList) {
             questionDao.storeOne(question);
         }
-
+        System.out.println("Vragen succesvol toegevoegd");
     }
 
-    private static void  bulkCreateCourses() throws SQLException {
-        CourseDAO courseDao = new CourseDAO(dbAccess);
-        List<Course> courses = generateCsvListToCourses(convertCsvToArray("resources/Cursussen.csv"));
-        courseDao.bulkCreate(courses);
-        System.out.println("Cursussen succesvol toegevoegd");
-    }*/
+    private static void bulkCreateQuizzes() throws SQLException {
+        String locationQuestionCSV = "Resources/Quizzen.csv";
+        QuizDAO quizDAO = new QuizDAO(dbAccess);
+        List<String> quizInCsv = convertCsvToArray(locationQuestionCSV);
+        List<Quiz> quizList = convertQuizListToObjects(quizInCsv);
+        for (Quiz quiz : quizList) {
+            quizDAO.storeOne(quiz);
+        }
+        System.out.println("Quizzen succesvol toegevoegd");
+    }
+
+    private static void bulkCreateUsers() throws SQLException, IOException {
+        UserDAO userDAO = new UserDAO(dbAccess);
+        // Small CSV to check import into Java.
+        String csvFilePath = "resources/Gebruikers.csv";
+        List<User> importUsersTest = convertListToUsers(csvFilePath);
+        // For loop to import all users to the DB.
+        for (User user : importUsersTest) {
+            userDAO.storeOne(user);
+        }
+        System.out.println("Gebruikers succesvol toegevoegd");
+    }
 }
