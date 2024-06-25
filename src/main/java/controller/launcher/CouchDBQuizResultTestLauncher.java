3b833ace5b8bd93f54e95b2ebeccfa9c48a9980d
Zahir Ekrem <63649974+zahirekrem09@users.noreply.github.com>
2024-06-25 11:42:06 +0200

    fix : conflict

--- src/main/java/controller/launcher/CouchDBQuizResultTestLauncher.java
+++ src/main/java/controller/launcher/CouchDBQuizResultTestLauncher.java
@@ -1,9 +1,13 @@
 package controller.launcher;
 
 import database.couchdb.CouchDBaccess;
+import database.couchdb.CourseCouchDBDAO;
 import database.couchdb.QuizResultCouchDBDAO;
+import database.mysql.CourseDAO;
+import database.mysql.DBAccess;
+import database.mysql.QuizDAO;
+import database.mysql.UserDAO;
 import model.QuizResult;
-import model.QuizResultDTO;
 import view.Main;
 
 import java.util.List;
@@ -13,22 +17,24 @@ import java.util.List;
  * @project quizmaster
  * @created 22 June Saturday 2024 - 17:52
  */
-public class CouchDBQuizResultTestLauncher {
+public class CouchDBQuizREsultTestLauncher {
 
-    private static final CouchDBaccess couchDBaccess = Main.getCouchDBaccess();
+    private static final CouchDBaccess couchDBaccess= Main.getCouchDBaccess();
+    private static final DBAccess dbAccess= Main.getdBaccess();
+
     private static final QuizResultCouchDBDAO quizResultCouchDBDAO = new QuizResultCouchDBDAO(couchDBaccess);
-
+    private static final UserDAO userDAO = new UserDAO(dbAccess);
+    private static final QuizDAO quizDAO = new QuizDAO(dbAccess);
 
     public static void main(String[] args) {
         // * create quiz result with quiz and user fixme @Ekrem : resulId auto increment
-        QuizResultDTO quizResult = new QuizResultDTO(1, 1, 1, 12);
-        QuizResultDTO quizResult2 = new QuizResultDTO(1, 3, 1, 12);
+        QuizResult quizResult = new QuizResult(1,userDAO.getById(1),quizDAO.getById(1),12 );
+        QuizResult quizResult2 = new QuizResult(1,userDAO.getById(3),quizDAO.getById(1),12 );
         quizResultCouchDBDAO.saveQuizResult(quizResult);
         quizResultCouchDBDAO.saveQuizResult(quizResult2);
-        List<QuizResult> quizResults = quizResultCouchDBDAO.getAllQuizResults();
-        for (QuizResult qr : quizResults) {
-            System.out.println(qr.getDate());
-        }
+
+        List<QuizResult> quizResults = quizResultCouchDBDAO.getAllQuizResults(1,1);
+        System.out.println(quizResults.size());
     }
 
 
